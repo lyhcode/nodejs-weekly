9c5de3982aec6de3475cac955c4a2981d378412d
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2012-06-24 06:52:52 +0800

    first commit

---
+++ app.coffee
@@ -0,0 +1,52 @@
+###
+Module dependencies.
+###
+
+express = require('express')
+routes = require('./routes')
+
+app = module.exports = express.createServer()
+
+# Stylus
+
+bootstrap = require('bootstrap-stylus')
+stylus = require('stylus')
+
+compile = (str, path) ->
+    stylus(str)
+        .set('filename', path)
+        .set('compress', true)
+        .use(bootstrap())
+
+app.use stylus.middleware(
+  src: __dirname + '/public'
+  compile: compile
+)
+
+# Configuration
+
+app.configure ->
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'jade')
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  app.use(app.router)
+  app.use(express.static(__dirname + '/public'))
+
+app.configure 'development', ->
+    app.use express.errorHandler(
+        dumpExceptions: true
+        showStack: true
+    )
+
+app.configure 'production', ->
+    app.use express.errorHandler()
+
+# Routes
+
+app.get('/', routes.index)
+
+app.listen 8080
+
+console.log "Express server listening in #{app.settings.env} mode"
+# console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ app.js
@@ -0,0 +1,54 @@
+
+/*
+Module dependencies.
+*/
+
+(function() {
+  var app, bootstrap, compile, express, routes, stylus;
+
+  express = require('express');
+
+  routes = require('./routes');
+
+  app = module.exports = express.createServer();
+
+  bootstrap = require('bootstrap-stylus');
+
+  stylus = require('stylus');
+
+  compile = function(str, path) {
+    return stylus(str).set('filename', path).set('compress', true).use(bootstrap());
+  };
+
+  app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+  }));
+
+  app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    return app.use(express.static(__dirname + '/public'));
+  });
+
+  app.configure('development', function() {
+    return app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
+  });
+
+  app.configure('production', function() {
+    return app.use(express.errorHandler());
+  });
+
+  app.get('/', routes.index);
+
+  app.listen(8080);
+
+  console.log("Express server listening in " + app.settings.env + " mode");
+
+}).call(this);
